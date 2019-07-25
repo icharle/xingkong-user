47914ba638e0969ae48f9d52a42d86b21aff6842
icharle <icharle@live.com>
2019-07-25 16:29:22 +0800

    初步项目架子

---
+++ xingkong-user-biz/src/main/java/us/xingkong/user/biz/Application.java
@@ -0,0 +1,17 @@
+package us.xingkong.user.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Icharle
+ * @Date: 2019-07-25 16:22
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
